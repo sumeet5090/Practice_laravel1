c4675b431c80d87e4aa60e641b2de070a7a006e1
sumeet5090 <58212406+sumeet5090@users.noreply.github.com>
2022-09-25 12:28:41 +0530

    commiting laravel files

---
+++ database/migrations/2022_09_25_062641_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->string('excerpt');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
